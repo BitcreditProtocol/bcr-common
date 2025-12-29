65d56e7d4b951442d80d5b6a82182f1c3c14d1fa
Stefan <stefanbitcr@gmail.com>
2025-12-29 14:20:21 +0200

    Review fixes

--- src/wire/melt.rs
+++ src/wire/melt.rs
@@ -1,20 +1,24 @@
 // ----- standard library imports
 // ----- extra library imports
-use cashu::{Amount, CurrencyUnit, nuts::MeltOptions};
+use bitcoin::{Address, Amount, address::NetworkUnchecked};
+use cashu::{CurrencyUnit, nuts::MeltOptions};
 use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
 // ----- local imports
 // ----- end imports
 
-#[derive(Debug, Clone, Hash, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq, Serialize, Deserialize, ToSchema)]
 pub struct OnchainInvoice {
     /// Total amount
+    #[schema(value_type = u64)]
     pub amount: Amount,
     /// Bitcoin address to pay
-    pub address: bitcoin::Address<bitcoin::address::NetworkUnchecked>,
+    #[schema(value_type = String)]
+    pub address: Address<NetworkUnchecked>,
 }
 
 /// Onchain Melt quote request
-#[derive(Debug, Clone, Hash, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq, Serialize, Deserialize, ToSchema)]
 pub struct MeltQuoteOnchainRequest {
     /// Bitcoin Address
     pub request: OnchainInvoice,
@@ -23,5 +27,6 @@ pub struct MeltQuoteOnchainRequest {
     /// Payment Options
     pub options: Option<MeltOptions>,
     /// Wallet signature
+    #[schema(value_type = String)]
     pub signature: bitcoin::secp256k1::schnorr::Signature,
 }
--- src/wire/mint.rs
+++ src/wire/mint.rs
@@ -1,27 +1,33 @@
 // ----- standard library imports
 // ----- extra library imports
-use cashu::{Amount, CurrencyUnit};
+use bitcoin::{Address, Amount, address::NetworkUnchecked};
+use cashu::CurrencyUnit;
 use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
 // ----- local imports
 // ----- end imports
 
 /// Onchain Mint quote request
-#[derive(Debug, Clone, Hash, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq, Serialize, Deserialize, ToSchema)]
 pub struct MintQuoteOnchainRequest {
     /// Amount to send and mint
+    #[schema(value_type = u64)]
     pub amount: Amount,
     /// Unit wallet would like to receive
     pub unit: CurrencyUnit,
 }
 
 /// Onchain Mint quote response
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
 pub struct MintQuoteOnchainResponse {
     /// Quote ID (UUID v4)
+    #[schema(value_type = String)]
     pub quote: uuid::Uuid,
     /// Bitcoin address to send payment
-    pub address: bitcoin::Address<bitcoin::address::NetworkUnchecked>,
+    #[schema(value_type = String)]
+    pub address: Address<NetworkUnchecked>,
     /// Amount received
+    #[schema(value_type = u64)]
     pub amount: Amount,
     /// Expiry timestamp
     pub expiry: u64,
