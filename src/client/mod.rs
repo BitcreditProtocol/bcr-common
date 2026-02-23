10bbb83d2a3fee13ce7299171ec6a9f2c3feb5d5
codingpeanut157 <codingpeanut15@gmail.com>
2026-02-23 16:51:23 +0100

    [client] merge keys and swap clients into core

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -5,9 +5,12 @@
 mod authorization;
 pub mod cdk;
 pub mod clowder;
+pub mod core;
 pub mod ebill;
+#[deprecated(since = "0.8.0", note = "Use crate::client::core instead")]
 pub mod keys;
 pub mod quote;
+#[deprecated(since = "0.8.0", note = "Use crate::client::core instead")]
 pub mod swap;
 
 // ----- end imports
