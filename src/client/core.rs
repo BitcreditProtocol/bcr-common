5a82a93e7e2e94bbcbf734c5caebb447945aaa82
codingpeanut157 <codingpeanut15@gmail.com>
2026-02-24 22:33:06 +0100

    [client] add treasury client
    we also move the credit minting APIs to the treasury

--- src/client/core.rs
+++ src/client/core.rs
@@ -7,20 +7,15 @@ use crate::wire::{keys as wire_keys, swap as wire_swap};
 // ----- end imports
 
 pub type Result<T> = std::result::Result<T, Error>;
-
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("resource not found {0}")]
     KeysetIdNotFound(cashu::Id),
-    #[error("mint operation not found {0}")]
-    MintOpNotFound(uuid::Uuid),
     #[error("invalid request")]
     InvalidRequest,
 
     #[error("internal error {0}")]
     Reqwest(#[from] reqwest::Error),
-    #[error("sign error {0}")]
-    NUT20(#[from] cashu::nut20::Error),
 }
 
 #[derive(Debug, Clone)]
@@ -155,93 +150,6 @@ impl Client {
         let kid = response.json::<cashu::Id>().await?;
         Ok(kid)
     }
-
-    pub const NEWMINTOP_EP_V1: &'static str = "/v1/admin/keys/mintop";
-    pub async fn new_mint_operation(
-        &self,
-        qid: uuid::Uuid,
-        kid: cashu::Id,
-        pk: cashu::PublicKey,
-        target: cashu::Amount,
-        bill_id: crate::core::BillId,
-    ) -> Result<()> {
-        let url = self
-            .base
-            .join(Self::NEWMINTOP_EP_V1)
-            .expect("mint operation relative path");
-        let msg = wire_keys::NewMintOperationRequest {
-            quote_id: qid,
-            kid,
-            pub_key: pk,
-            target,
-            bill_id,
-        };
-        let request = self.cl.post(url).json(&msg);
-        let response = request.send().await?;
-        if response.status() == reqwest::StatusCode::NOT_FOUND {
-            return Err(Error::KeysetIdNotFound(kid));
-        }
-        let _response = response
-            .json::<wire_keys::NewMintOperationResponse>()
-            .await?;
-        Ok(())
-    }
-
-    pub const MINTOPSTATUS_EP_V1: &'static str = "/v1/admin/keys/mintop/{qid}";
-    pub async fn mint_operation_status(
-        &self,
-        qid: uuid::Uuid,
-    ) -> Result<wire_keys::MintOperationStatus> {
-        let url = self
-            .base
-            .join(&Self::MINTOPSTATUS_EP_V1.replace("{qid}", &qid.to_string()))
-            .expect("mint operation status relative path");
-        let request = self.cl.get(url);
-        let response = request.send().await?;
-        if response.status() == reqwest::StatusCode::NOT_FOUND {
-            return Err(Error::MintOpNotFound(qid));
-        }
-        let response = response.json::<wire_keys::MintOperationStatus>().await?;
-        Ok(response)
-    }
-
-    pub const LISTMINTOPS_EP_V1: &'static str = "/v1/admin/keys/mintops/{kid}";
-    pub async fn list_mint_operations(&self, kid: cashu::Id) -> Result<Vec<uuid::Uuid>> {
-        let url = self
-            .base
-            .join(&Self::LISTMINTOPS_EP_V1.replace("{kid}", &kid.to_string()))
-            .expect("list mint operations relative path");
-        let request = self.cl.get(url);
-        let response = request.send().await?;
-        let response = response.json::<Vec<uuid::Uuid>>().await?;
-        Ok(response)
-    }
-
-    pub const MINT_EP_V1: &'static str = "/v1/mint/ebill";
-    pub async fn mint(
-        &self,
-        qid: uuid::Uuid,
-        outputs: Vec<cashu::BlindedMessage>,
-        sk: cashu::SecretKey,
-    ) -> Result<Vec<cashu::BlindSignature>> {
-        let url = self
-            .base
-            .join(Self::MINT_EP_V1)
-            .expect("mint relative path");
-        let mut msg = cashu::MintRequest {
-            quote: qid,
-            outputs,
-            signature: None,
-        };
-        msg.sign(sk)?;
-        let result = self.cl.post(url).json(&msg).send().await?;
-        if result.status() == reqwest::StatusCode::NOT_FOUND {
-            return Err(Error::MintOpNotFound(qid));
-        }
-        let response = result.json::<cashu::MintResponse>().await?;
-        Ok(response.signatures)
-    }
-
     pub const RESTORE_EP_V1: &'static str = "/v1/restore";
     pub async fn restore(
         &self,
