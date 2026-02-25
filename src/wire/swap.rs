1e84602e6152e95976e211048db97a40f3b9ca97
Stefan <stefanbitcr@gmail.com>
2026-02-25 09:08:37 +0100

    Add to schema

--- src/wire/swap.rs
+++ src/wire/swap.rs
@@ -80,55 +80,55 @@ pub struct ForceSwapRequest {
     pub commitment: bitcoin::secp256k1::schnorr::Signature,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
 pub struct AccumulatorWitness {
     pub root: [u8; 32],
     pub inputs: Vec<AccumulatorWitnessInput>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
 pub struct AccumulatorWitnessInput {
     pub leaf_index: usize,
     pub path: Vec<([u8; 32], bool)>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
 pub struct StarkAccumulatorProof {
     pub root: [u8; 32],
     pub proof_bytes: Vec<u8>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
 pub struct SwapRequest {
     pub inputs: Vec<cashu::Proof>,
     pub outputs: Vec<cashu::BlindedMessage>,
     pub accumulator_proof: Vec<u8>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, ToSchema)]
 pub struct SwapResponse {
     pub signatures: Vec<cashu::nuts::BlindSignature>,
     pub accumulator_witness: AccumulatorWitness,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, ToSchema)]
 pub struct MintWithWitnessResponse {
     pub signatures: Vec<cashu::nuts::BlindSignature>,
     pub accumulator_witness: AccumulatorWitness,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, ToSchema)]
 pub struct AccumulatorInfoResponse {
     pub leaf_count: usize,
     pub root: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, ToSchema)]
 pub struct AccumulatorPathsRequest {
     pub indices: Vec<usize>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, ToSchema)]
 pub struct AccumulatorPathsResponse {
     pub root: String,
     pub paths: Vec<Option<Vec<(String, bool)>>>,
