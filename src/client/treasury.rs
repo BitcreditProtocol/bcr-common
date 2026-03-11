b1fd94675438d3c22b79196c2bbe0ce64aae3ee9
codingpeanut157 <codingpeanut15@gmail.com>
2026-03-11 15:44:20 +0100

    [client] treasury remove unused endpoints

--- src/client/treasury.rs
+++ src/client/treasury.rs
@@ -362,6 +362,4 @@ impl Client {
     pub const MELTQUOTE_ONCHAIN_EP_V1: &'static str = "/v1/melt/quote/onchain";
     pub const MELT_ONCHAIN_EP_V1: &'static str = "/v1/melt/onchain";
     pub const MINTQUOTE_ONCHAIN_EP_V1: &'static str = "/v1/mint/quote/onchain";
-    pub const MINTQUOTE_ONCHAIN_GET_EP_V1: &'static str = "/v1/mint/quote/onchain/{quote_id}";
-    pub const MINT_ONCHAIN_EP_V1: &'static str = "/v1/mint/onchain";
 }
