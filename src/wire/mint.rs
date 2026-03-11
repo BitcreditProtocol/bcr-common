8eb8bd8bc2c88dde01f5d73f6fb64243e6b8274f
codingpeanut157 <codingpeanut15@gmail.com>
2026-03-11 15:44:51 +0100

    [wire] minor fixes to mint messages

--- src/wire/mint.rs
+++ src/wire/mint.rs
@@ -1,10 +1,15 @@
 // ----- standard library imports
 // ----- extra library imports
-use bitcoin::{Address, Amount, address::NetworkUnchecked};
+use bitcoin::Amount;
+use borsh::{BorshDeserialize, BorshSerialize};
 use cashu::CurrencyUnit;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 // ----- local imports
+use crate::wire::borsh::{
+    deserialize_btc_amount, deserialize_from_str, deserialize_vec_of_jsons, serialize_as_str,
+    serialize_btc_amount, serialize_vec_of_jsons,
+};
 // ----- end imports
 
 /// Onchain Mint quote request
@@ -20,30 +25,40 @@ pub struct MintQuoteOnchainRequest {
 }
 
 /// Onchain Mint quote response body
-#[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
+#[derive(Debug, Clone, Serialize, Deserialize, ToSchema, BorshSerialize, BorshDeserialize)]
 pub struct MintQuoteOnchainResponseBody {
     /// Quote ID (UUID v4)
     #[schema(value_type = String)]
+    #[borsh(
+        serialize_with = "serialize_as_str",
+        deserialize_with = "deserialize_from_str"
+    )]
     pub quote: uuid::Uuid,
     /// Bitcoin address to send payment
-    #[schema(value_type = String)]
-    pub address: Address<NetworkUnchecked>,
+    pub address: String,
     /// Amount received
     #[schema(value_type = u64)]
+    #[borsh(
+        serialize_with = "serialize_btc_amount",
+        deserialize_with = "deserialize_btc_amount"
+    )]
     pub amount: Amount,
     /// Expiry timestamp
     pub expiry: u64,
     /// Blinded messages committed to
+    #[borsh(
+        serialize_with = "serialize_vec_of_jsons",
+        deserialize_with = "deserialize_vec_of_jsons"
+    )]
     pub blinded_messages: Vec<cashu::nuts::BlindedMessage>,
 }
 
 /// Onchain Mint quote response with commitment signature
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
 pub struct MintQuoteOnchainResponse {
-    pub body: MintQuoteOnchainResponseBody,
+    pub content: String, // base64, borsh serialized MintQuoteOnchainResponseBody
     #[schema(value_type = String)]
     pub commitment: bitcoin::secp256k1::schnorr::Signature,
-    pub state: Option<cashu::MintQuoteState>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
