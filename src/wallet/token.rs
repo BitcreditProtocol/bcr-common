f367727e6c46c4782cc43db62ddb2ff0e01752df
Mario Zupan <mario@zupzup.org>
2026-06-08 15:00:31 +0200

    Remove cashuB Token Support (#140)

--- src/wallet/token.rs
+++ src/wallet/token.rs
@@ -10,8 +10,6 @@ use std::collections::HashMap;
 use std::fmt;
 use std::str::FromStr;
 
-pub type CashuTokenV4 = cashu::nut00::TokenV4;
-
 #[doc(hidden)]
 #[macro_export]
 macro_rules! ensure_cdk {
@@ -27,14 +25,12 @@ macro_rules! ensure_cdk {
 #[serde(tag = "type")]
 pub enum Token {
     BitcrV4(BitcrTokenV4),
-    CashuV4(CashuTokenV4),
 }
 
 impl fmt::Display for Token {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let token = match self {
             Self::BitcrV4(token) => token.to_string(),
-            Self::CashuV4(token) => token.to_string(),
         };
 
         write!(f, "{token}")
@@ -59,27 +55,10 @@ impl Token {
         })
     }
 
-    /// Create new cashuV4 [`Token`]
-    pub fn new_cashu(
-        mint_url: MintUrl,
-        proofs: Proofs,
-        memo: Option<String>,
-        unit: CurrencyUnit,
-    ) -> Self {
-        let proofs = proofs_to_tokenv4(proofs);
-
-        Self::CashuV4(CashuTokenV4 {
-            mint_url,
-            unit,
-            memo,
-            token: proofs,
-        })
-    }
     /// Proofs in [`Token`]
     pub fn proofs(&self, mint_keysets: &[KeySetInfo]) -> Result<Proofs, Error> {
         match self {
             Self::BitcrV4(token) => token.proofs(mint_keysets),
-            Self::CashuV4(token) => token.proofs(mint_keysets),
         }
     }
 
@@ -87,7 +66,6 @@ impl Token {
     pub fn value(&self) -> Result<Amount, Error> {
         match self {
             Self::BitcrV4(token) => token.value(),
-            Self::CashuV4(token) => token.value(),
         }
     }
 
@@ -95,7 +73,6 @@ impl Token {
     pub fn memo(&self) -> &Option<String> {
         match self {
             Self::BitcrV4(token) => token.memo(),
-            Self::CashuV4(token) => token.memo(),
         }
     }
 
@@ -103,7 +80,6 @@ impl Token {
     pub fn unit(&self) -> Option<CurrencyUnit> {
         match self {
             Self::BitcrV4(token) => Some(token.unit().clone()),
-            Self::CashuV4(token) => Some(token.unit().clone()),
         }
     }
 
@@ -111,15 +87,13 @@ impl Token {
     pub fn mint_url(&self) -> MintUrl {
         match self {
             Self::BitcrV4(token) => token.mint_url.clone(),
-            Self::CashuV4(token) => token.mint_url.clone(),
         }
     }
 
     /// Serialize the token to raw binary
     pub fn to_raw_bytes(&self) -> Result<Vec<u8>, Error> {
         match self {
-            Self::BitcrV4(_) => Err(Error::UnsupportedToken),
-            Self::CashuV4(token) => token.to_raw_bytes(),
+            Self::BitcrV4(token) => token.to_raw_bytes(),
         }
     }
 }
@@ -128,9 +102,8 @@ impl FromStr for Token {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match (CashuTokenV4::from_str(s), BitcrTokenV4::from_str(s)) {
-            (Ok(token), Err(_)) => Ok(Token::CashuV4(token)),
-            (Err(_), Ok(token)) => Ok(Token::BitcrV4(token)),
+        match BitcrTokenV4::from_str(s) {
+            Ok(token) => Ok(Token::BitcrV4(token)),
             _ => Err(Error::UnsupportedToken),
         }
     }
@@ -140,9 +113,6 @@ impl TryFrom<&Vec<u8>> for Token {
     type Error = Error;
 
     fn try_from(bytes: &Vec<u8>) -> Result<Self, Self::Error> {
-        if let Ok(token) = CashuTokenV4::try_from(bytes) {
-            return Ok(Token::CashuV4(token));
-        }
         if let Ok(token) = BitcrTokenV4::try_from(bytes) {
             return Ok(Token::BitcrV4(token));
         }
@@ -279,39 +249,12 @@ mod tests {
     use std::str::FromStr;
 
     #[test]
-    fn test_token_str_round_trip_1() {
-        let token_str = "cashuBpGF0gaJhaUgArSaMTR9YJmFwgaNhYQFhc3hAOWE2ZGJiODQ3YmQyMzJiYTc2ZGIwZGYxOTcyMTZiMjlkM2I4Y2MxNDU1M2NkMjc4MjdmYzFjYzk0MmZlZGI0ZWFjWCEDhhhUP_trhpXfStS6vN6So0qWvc2X3O4NfM-Y1HISZ5JhZGlUaGFuayB5b3VhbXVodHRwOi8vbG9jYWxob3N0OjMzMzhhdWNzYXQ=";
-
-        let token = Token::from_str(token_str).unwrap();
-        assert!(matches!(token, Token::CashuV4(_)));
-        let Token::CashuV4(inner) = token.clone() else {
-            panic!("Expected CashuV4 token");
-        };
-        assert_eq!(inner.token.len(), 1);
-        assert_eq!(inner.token[0].keyset_id.to_string(), "00ad268c4d1f5826");
-        let _ = token.to_string().strip_prefix("cashuB").expect("prefix");
-        assert_eq!(inner.mint_url.to_string(), "http://localhost:3338");
-        assert_eq!(
-            inner.token[0].keyset_id,
-            cdk02::ShortKeysetId::from_str("00ad268c4d1f5826").unwrap()
-        );
-        assert_eq!(inner.unit.clone(), cashu::CurrencyUnit::Sat);
-
-        let encoded = &inner.to_string();
-
-        let token_data = CashuTokenV4::from_str(encoded).unwrap();
-        assert_eq!(token_data, inner);
-    }
-
-    #[test]
-    fn test_token_str_round_trip_2() {
-        let token_str = "bitcrBpGFtdWh0dHA6Ly9sb2NhbGhvc3Q6MzMzOGF1ZWNyc2F0YXSBomFpSACtJoxNH1gmYXCBo2FhAWFzeEA5YTZkYmI4NDdiZDIzMmJhNzZkYjBkZjE5NzIxNmIyOWQzYjhjYzE0NTUzY2QyNzgyN2ZjMWNjOTQyZmVkYjRlYWNYIQOGGFQ_-2uGld9K1Lq83pKjSpa9zZfc7g18z5jUchJnkmFkaVRoYW5rIHlvdQ";
+    fn test_token_str_round_trip() {
+        let token_str = "bitcrBpGFtdWh0dHA6Ly9sb2NhbGhvc3Q6MzMzOGF1Y3NhdGF0gaJhaUgArSaMTR9YJmFwgaNhYQFhc3hAOWE2ZGJiODQ3YmQyMzJiYTc2ZGIwZGYxOTcyMTZiMjlkM2I4Y2MxNDU1M2NkMjc4MjdmYzFjYzk0MmZlZGI0ZWFjWCEDhhhUP_trhpXfStS6vN6So0qWvc2X3O4NfM-Y1HISZ5JhZGlUaGFuayB5b3U";
 
         let token = Token::from_str(token_str).unwrap();
         assert!(matches!(token, Token::BitcrV4(_)));
-        let Token::BitcrV4(inner) = token.clone() else {
-            panic!("Expected BitcrV4 token");
-        };
+        let Token::BitcrV4(inner) = token.clone();
         assert_eq!(inner.token.len(), 1);
         assert_eq!(inner.token[0].keyset_id.to_string(), "00ad268c4d1f5826");
 
@@ -321,16 +264,14 @@ mod tests {
             inner.token[0].keyset_id,
             cdk02::ShortKeysetId::from_str("00ad268c4d1f5826").unwrap()
         );
-        assert_eq!(
-            inner.unit.clone(),
-            cashu::CurrencyUnit::Custom(String::from("crsat"))
-        );
+        assert_eq!(inner.unit.clone(), cashu::CurrencyUnit::Sat);
 
         let encoded = &inner.to_string();
 
         let token_data = BitcrTokenV4::from_str(encoded).unwrap();
         assert_eq!(token_data, inner);
     }
+
     #[test]
     fn incorrect_tokens() {
         let incorrect_prefix = "casshuAeyJ0b2tlbiI6W3sibWludCI6Imh0dHBzOi8vODMzMy5zcGFjZTozMzM4IiwicHJvb2ZzIjpbeyJhbW91bnQiOjIsImlkIjoiMDA5YTFmMjkzMjUzZTQxZSIsInNlY3JldCI6IjQwNzkxNWJjMjEyYmU2MWE3N2UzZTZkMmFlYjRjNzI3OTgwYmRhNTFjZDA2YTZhZmMyOWUyODYxNzY4YTc4MzciLCJDIjoiMDJiYzkwOTc5OTdkODFhZmIyY2M3MzQ2YjVlNDM0NWE5MzQ2YmQyYTUwNmViNzk1ODU5OGE3MmYwY2Y4NTE2M2VhIn0seyJhbW91bnQiOjgsImlkIjoiMDA5YTFmMjkzMjUzZTQxZSIsInNlY3JldCI6ImZlMTUxMDkzMTRlNjFkNzc1NmIwZjhlZTBmMjNhNjI0YWNhYTNmNGUwNDJmNjE0MzNjNzI4YzcwNTdiOTMxYmUiLCJDIjoiMDI5ZThlNTA1MGI4OTBhN2Q2YzA5NjhkYjE2YmMxZDVkNWZhMDQwZWExZGUyODRmNmVjNjlkNjEyOTlmNjcxMDU5In1dfV0sInVuaXQiOiJzYXQiLCJtZW1vIjoiVGhhbmsgeW91LiJ9";
@@ -345,7 +286,7 @@ mod tests {
 
         assert!(no_prefix_token.is_err());
 
-        let correct_token = "cashuBo2F0gqJhaUgA_9SLj17PgGFwgaNhYQFhc3hAYWNjMTI0MzVlN2I4NDg0YzNjZjE4NTAxNDkyMThhZjkwZjcxNmE1MmJmNGE1ZWQzNDdlNDhlY2MxM2Y3NzM4OGFjWCECRFODGd5IXVW-07KaZCvuWHk3WrnnpiDhHki6SCQh88-iYWlIAK0mjE0fWCZhcIKjYWECYXN4QDEzMjNkM2Q0NzA3YTU4YWQyZTIzYWRhNGU5ZjFmNDlmNWE1YjRhYzdiNzA4ZWIwZDYxZjczOGY0ODMwN2U4ZWVhY1ghAjRWqhENhLSsdHrr2Cw7AFrKUL9Ffr1XN6RBT6w659lNo2FhAWFzeEA1NmJjYmNiYjdjYzY0MDZiM2ZhNWQ1N2QyMTc0ZjRlZmY4YjQ0MDJiMTc2OTI2ZDNhNTdkM2MzZGNiYjU5ZDU3YWNYIQJzEpxXGeWZN5qXSmJjY8MzxWyvwObQGr5G1YCCgHicY2FtdWh0dHA6Ly9sb2NhbGhvc3Q6MzMzOGF1Y3NhdA==";
+        let correct_token = "bitcrBo2F0gqJhaUgA_9SLj17PgGFwgaNhYQFhc3hAYWNjMTI0MzVlN2I4NDg0YzNjZjE4NTAxNDkyMThhZjkwZjcxNmE1MmJmNGE1ZWQzNDdlNDhlY2MxM2Y3NzM4OGFjWCECRFODGd5IXVW-07KaZCvuWHk3WrnnpiDhHki6SCQh88-iYWlIAK0mjE0fWCZhcIKjYWECYXN4QDEzMjNkM2Q0NzA3YTU4YWQyZTIzYWRhNGU5ZjFmNDlmNWE1YjRhYzdiNzA4ZWIwZDYxZjczOGY0ODMwN2U4ZWVhY1ghAjRWqhENhLSsdHrr2Cw7AFrKUL9Ffr1XN6RBT6w659lNo2FhAWFzeEA1NmJjYmNiYjdjYzY0MDZiM2ZhNWQ1N2QyMTc0ZjRlZmY4YjQ0MDJiMTc2OTI2ZDNhNTdkM2MzZGNiYjU5ZDU3YWNYIQJzEpxXGeWZN5qXSmJjY8MzxWyvwObQGr5G1YCCgHicY2FtdWh0dHA6Ly9sb2NhbGhvc3Q6MzMzOGF1Y3NhdA==";
 
         let correct_token = Token::from_str(correct_token);
 
@@ -354,29 +295,18 @@ mod tests {
 
     #[test]
     fn test_token_value() {
-        let token_str = "bitcrBo2FtdWh0dHA6Ly9sb2NhbGhvc3Q6NDM0M2F1ZWNyc2F0YXSBomFpSABp3j5af6uYYXCHpGFhGEBhc3hAODcyYmIxNzY0ODA3NDY2YWUxMDY2MGQxMjA5ODUxYzQ2MGJmZjJmNDZiY2YyZmJmM2QzY2NjY2QyYzllMzNiMGFjWCECgISwm2AJEFh3vxZKCNjnxx3pZ8BBav7a5AXLtMVQVjRhZPakYWEYgGFzeEBhY2QzYzI5YjlhZjEwYmM4MTdiOWUxNGFhMjllZjIxODkzYmZjZWMwMzFmYWQyM2IxOWExMDhjMzFhZmQyODMyYWNYIQIMmOnUpdbYTBtRceuCXy_qajysL6sG9CsvtRSBukjWO2Fk9qRhYRkCAGFzeEA4ZmU1NDNmOTMxYjA4MzhhOTA3NmMyMjljNzg1OWU3MTc0MTUzMGVmMGFiZWMyMzlkOWE0ZWNjOGEyMGNlYzRmYWNYIQPqj23wVNNNx42KP28By2a5i6N5TMkVU8lixcZ3aeiA7WFk9qRhYQRhc3hAMzk4YjYzMmU4MTZmNzQ4Njc1N2E3NTk5Mzc2YjlhYmFkMGFmNGQwMTVkYTQ0Mjk5Zjg2OGYxNWM4ODdmNDNjYmFjWCEDo8X2Y4JoRJ1hGSXDSVgQH-YXpFw_NYXtPIUv5xJcX-9hZPakYWEIYXN4QGJjNjM4NTYxN2Q2NjJkN2Q5NWIxNDBlMTU4Y2MzMTYwZjAzMmQxMWJiZGEzZWY3MDRhYzcyOTliM2EzYjQyOThhY1ghA_UAeY1dWx5QHqsvepcUK68xfHZJIbuRCaM45uN4t9vsYWT2pGFhGQEAYXN4QDFlNGQ1ZGI1MTc2MzU2YWEwZTI2MzJmZDlkYTUxMjYzYmY1M2EyMjFkNmNhZmE5Y2U4YTExMjg4MGNhMWQwZmZhY1ghAm3brXrx4F8HY8-YeC-msEuI9vfSzBKayKzab58A6xYwYWT2pGFhAWFzeEAwNzcyNTMyYTJkMjZkNDcyOTZjNzQ3NzMxN2NhZjQzOTdjZjA4MmM0ZjkwMzE4YWJjMDljZGRmZTEyMzFiYThlYWNYIQPeNBo_DX-qSXr52rqbwhGKWx9VNpaddKwORBP9-43JzmFk9g==";
+        let token_str = "bitcrBpGFteC9odHRwczovL21pbnQud2lsZGNhdDAuY2xvd2Rlci1kZXYubWluaWJpbGwudGVjaGF1Y3NhdGFkY3NhdGF0gaJhaUgBRjxG54nx4WFwh6RhYQRhc3hAYjdkNTUwZjljZjYyYzk0NmM3YzdjNDVkNjc4ZmUxMzc3OTdkNWRkMDMwZjQxMWY1NDg2OTU4MjE1MjRkYWY0M2FjWCECy1iLQBVPbznqF_cuQ_hj7sVg39ZGE-4aFvTnkPPIyMhhZKNhZVggmwzCLAxNKeNsJw_ZM-n1nfyE1bQSpXB9rvE8sLjGK_xhc1ggmYxKXPHd1N8yIRYn7KCa6Jl28EMVsQF-QwJHxXHe34thclggXUw1Z18UJiVzDdi0soIgil4JGA6iBRBHDeK_Qy1tYFSkYWEYgGFzeEAxMmJiNjg2YTAwNTg5N2MyYmRkZTBkODQxMzE0Mjg2NmIzMzkwZWI5Zjk5NzYxYmQ2NzVhYzI3YmY3NTFiZWI3YWNYIQP_o6JSVmMSfJT5I6fDZH7iZUXsLxmthXXtlSA8oVI3gGFko2FlWCCPbRLKnGTc1_WD_a91Fg-rr8P8cYk2gTZ8Qk19HJYWwWFzWCBLUfhonMlxUp4VVi-nxr-vDPag-o2ZZx0c-W7d3OSlHGFyWCCWjjuYWMj05LXrMebjwod7LWDYEVbskxEvAghk_GvCxqRhYQhhc3hAOTY0ZGYwOWQwOGQ5ZDRjY2E0YmVmYmM4ZWUzYjA4YWM5NTQ1ZjcxOTAwYWNkYzIzZWIzZDExZjUwNzIxZTFlN2FjWCEDRsrtPoGEFuuwRPhNSDKMGCZSuhPNQjh6bzuq_LTOrKdhZKNhZVggtp564XxyxpkRS_TirpegGZHyWXm4v4OAUTmzLO9ktnZhc1gg4fA_a2epcYSprnk4WAIyFNwkvuJCRsRvGEKpDHvw8RNhclggcLgcKktVlCP0ILVz53sP6tWK5Zn2p_0maxSMlD0-3jakYWEYQGFzeEA4NmJlNDBlNzRlOGVjYjc3YzA4NTdmYWEzMWY4MGQwZTQ1YzNlZjEzNzNjMTBjZWI1NWQzMzFhY2FhOWE5NjA0YWNYIQPDD9NSpAzTe8Aii_xweeyw2oa9nA5zz2CR4QdlYcr8JmFko2FlWCCuy6WOHlp4S-UHUIkQjaa4sNw8zkLBCSv-DMl8X6ASTWFzWCBvrg90GrXfBzLnWg4E0T8CA0jA5hVuHAVrLKZ9kLteq2FyWCCIP-6v7_r1nO1WblfP-Mkf6D_hL7ELH7HyzkURBxEhCKRhYQFhc3hAZmI3YjEwMzFiMjRkNDBjODBjZTA1MjNkNGM0YzQxYTQ3MWY4OGI3ZWIzYTk1OWVlZDA4ZjQ0YWVjYjdhNThjNGFjWCEDcX4YKFg8E2RWOMbJzWfm-f-0mlg89Cuy0ZFDYSB5-a1hZKNhZVggQJ2jupPgw1S-NMqPdCTq8OkhjOsolRfBN5unMlWlQTZhc1ggAssEObaR2OZEwXj01Q7GMQ4xfaENrggLxU48minMRtVhclgg2SjONoi2MlTIF9zUhXAbudms-8CLSpI8cVwl_f23dRCkYWEZAgBhc3hAODY1NjNkN2RjNzYzYjZkN2MxYjQ4MWJlMWIwMzY5OTRhNjFlMGZmNDU2MWZmZGJlMzQ1ZDc4NjM5ZTJmM2M2OGFjWCEDmaQqK0Ng1ou8FFpzezyjYuJ3VfvAqh_fURHANFya7yxhZKNhZVggCP8wUozvBK7B89fpGmkLC4dXGJbaf919pnND5zXAo1Fhc1ggAMn3Zb1qlMCUQhpsiSnBmFQDBUa8RCEuGxWamPSTfKlhclggjzGTRZcZfx6V7b3Qds7p-q-yipRB2x2DVG2Y2rGmu1qkYWEZAQBhc3hANDY3MDM3OTgxNGMzM2Y3OTRmNTdhOTdjNTQ5ZTFmZDE1YTQyYTE4NWVjZTM2MGFmN2ZkYjliMTYxOTMxN2YxZmFjWCED9QgfGg6NNmI27aVQTGIGOD_AOvCv6ULNk8Gls3gb-YthZKNhZVggXGVixAC9eS6SITqMI2eM6yDdFbXR6idgIUXBkZA9qxthc1ggrb-pKZrAjxBl-BI-IwEvzvChGoSdFFyKMNhCGkB8h0JhclggKyY-ukPdK5o3RcsOGcM44L3KnG_lG4z2CupyqkvWqLk=";
 
         let token = Token::from_str(token_str).unwrap();
         token.to_string().strip_prefix("bitcrB").unwrap();
-        if let Token::BitcrV4(token) = token {
-            assert_eq!(token.value().unwrap(), cashu::Amount::from(973));
-            assert_eq!(token.unit.to_string(), "crsat");
-        }
+        let Token::BitcrV4(token) = token;
+        assert_eq!(token.value().unwrap(), cashu::Amount::from(973));
+        assert_eq!(token.unit.to_string(), "sat");
     }
 
     #[test]
     fn test_serde() {
         let token_str = "bitcrBo2FtdWh0dHA6Ly9sb2NhbGhvc3Q6NDM0M2F1ZWNyc2F0YXSBomFpSABp3j5af6uYYXCHpGFhGEBhc3hAODcyYmIxNzY0ODA3NDY2YWUxMDY2MGQxMjA5ODUxYzQ2MGJmZjJmNDZiY2YyZmJmM2QzY2NjY2QyYzllMzNiMGFjWCECgISwm2AJEFh3vxZKCNjnxx3pZ8BBav7a5AXLtMVQVjRhZPakYWEYgGFzeEBhY2QzYzI5YjlhZjEwYmM4MTdiOWUxNGFhMjllZjIxODkzYmZjZWMwMzFmYWQyM2IxOWExMDhjMzFhZmQyODMyYWNYIQIMmOnUpdbYTBtRceuCXy_qajysL6sG9CsvtRSBukjWO2Fk9qRhYRkCAGFzeEA4ZmU1NDNmOTMxYjA4MzhhOTA3NmMyMjljNzg1OWU3MTc0MTUzMGVmMGFiZWMyMzlkOWE0ZWNjOGEyMGNlYzRmYWNYIQPqj23wVNNNx42KP28By2a5i6N5TMkVU8lixcZ3aeiA7WFk9qRhYQRhc3hAMzk4YjYzMmU4MTZmNzQ4Njc1N2E3NTk5Mzc2YjlhYmFkMGFmNGQwMTVkYTQ0Mjk5Zjg2OGYxNWM4ODdmNDNjYmFjWCEDo8X2Y4JoRJ1hGSXDSVgQH-YXpFw_NYXtPIUv5xJcX-9hZPakYWEIYXN4QGJjNjM4NTYxN2Q2NjJkN2Q5NWIxNDBlMTU4Y2MzMTYwZjAzMmQxMWJiZGEzZWY3MDRhYzcyOTliM2EzYjQyOThhY1ghA_UAeY1dWx5QHqsvepcUK68xfHZJIbuRCaM45uN4t9vsYWT2pGFhGQEAYXN4QDFlNGQ1ZGI1MTc2MzU2YWEwZTI2MzJmZDlkYTUxMjYzYmY1M2EyMjFkNmNhZmE5Y2U4YTExMjg4MGNhMWQwZmZhY1ghAm3brXrx4F8HY8-YeC-msEuI9vfSzBKayKzab58A6xYwYWT2pGFhAWFzeEAwNzcyNTMyYTJkMjZkNDcyOTZjNzQ3NzMxN2NhZjQzOTdjZjA4MmM0ZjkwMzE4YWJjMDljZGRmZTEyMzFiYThlYWNYIQPeNBo_DX-qSXr52rqbwhGKWx9VNpaddKwORBP9-43JzmFk9g==";
-
-        let token = Token::from_str(token_str).unwrap();
-        let token_json = serde_json::to_string(&token).unwrap();
-        let deserialized_token: Token = serde_json::from_str(&token_json).unwrap();
-        assert_eq!(token, deserialized_token);
-    }
-
-    #[test]
-    fn test_serde_2() {
-        let token_str = "cashuBo2F0gqJhaUgA_9SLj17PgGFwgaNhYQFhc3hAYWNjMTI0MzVlN2I4NDg0YzNjZjE4NTAxNDkyMThhZjkwZjcxNmE1MmJmNGE1ZWQzNDdlNDhlY2MxM2Y3NzM4OGFjWCECRFODGd5IXVW-07KaZCvuWHk3WrnnpiDhHki6SCQh88-iYWlIAK0mjE0fWCZhcIKjYWECYXN4QDEzMjNkM2Q0NzA3YTU4YWQyZTIzYWRhNGU5ZjFmNDlmNWE1YjRhYzdiNzA4ZWIwZDYxZjczOGY0ODMwN2U4ZWVhY1ghAjRWqhENhLSsdHrr2Cw7AFrKUL9Ffr1XN6RBT6w659lNo2FhAWFzeEA1NmJjYmNiYjdjYzY0MDZiM2ZhNWQ1N2QyMTc0ZjRlZmY4YjQ0MDJiMTc2OTI2ZDNhNTdkM2MzZGNiYjU5ZDU3YWNYIQJzEpxXGeWZN5qXSmJjY8MzxWyvwObQGr5G1YCCgHicY2FtdWh0dHA6Ly9sb2NhbGhvc3Q6MzMzOGF1Y3NhdA==";
 
         let token = Token::from_str(token_str).unwrap();
         let token_json = serde_json::to_string(&token).unwrap();
