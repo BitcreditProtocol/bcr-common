1fc5f98236e709d20cef5f8dcde655831c38cacf
codingpeanut157 <codingpeanut15@gmail.com>
2026-08-12 17:46:28 +0200

    [wire] add some derive directives to AddReserveStatus

--- src/wire/clowder.rs
+++ src/wire/clowder.rs
@@ -721,7 +721,13 @@ pub struct AddReserveResponse {
     pub status: AddReserveStatus,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, ToSchema, PartialEq, Eq)]
+#[derive(
+    Debug, Clone, Serialize, Deserialize, ToSchema, PartialEq, Eq, strum::EnumDiscriminants,
+)]
+#[strum_discriminants(
+    derive(strum::Display, strum::EnumString),
+    strum(serialize_all = "lowercase")
+)]
 pub enum AddReserveStatus {
     Pending,
     Completed {
