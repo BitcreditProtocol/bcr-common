7868d9a23b04d6e5a0ef3d06830e41eab8bfaa27
codingpeanut157 <codingpeanut15@gmail.com>
2026-09-10 16:17:01 +0200

    [ecash] introduce cashu::Id equivalent

--- src/ecash.rs
+++ src/ecash.rs
@@ -1,6 +1,11 @@
 // ----- standard library imports
+use std::str::FromStr;
 // ----- extra library imports
-use bitcoin::bip32 as btc32;
+use bitcoin::{
+    bip32 as btc32,
+    hex::{DisplayHex, FromHex},
+    secp256k1 as secp,
+};
 use borsh::{BorshDeserialize, BorshSerialize};
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
@@ -18,11 +23,87 @@ pub enum Error {
     Dhke(#[from] cashu::dhke::Error),
     #[error("amount {0}")]
     Amount(#[from] cashu::amount::Error),
+    #[error("invalid keyset id")]
+    InvalidKeysetId,
 }
 
-#[derive(
-    Debug, Clone, PartialEq, Serialize, Deserialize, ToSchema, BorshSerialize, BorshDeserialize,
-)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Id {
+    V1([u8; 7]),
+    V2([u8; 32]),
+}
+
+impl std::fmt::Display for Id {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::V1(body) => write!(f, "00{}", body.as_hex()),
+            Self::V2(body) => write!(f, "01{}", body.as_hex()),
+        }
+    }
+}
+
+impl FromStr for Id {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        match s.split_at_checked(2) {
+            Some(("00", body)) => Ok(Self::V1(
+                FromHex::from_hex(body).map_err(|_| Error::InvalidKeysetId)?,
+            )),
+            Some(("01", body)) => Ok(Self::V2(
+                FromHex::from_hex(body).map_err(|_| Error::InvalidKeysetId)?,
+            )),
+            _ => Err(Error::InvalidKeysetId),
+        }
+    }
+}
+
+impl serde::Serialize for Id {
+    fn serialize<S>(&self, s: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        s.collect_str(self)
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for Id {
+    fn deserialize<D>(d: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = <std::string::String as serde::Deserialize>::deserialize(d)?;
+        Id::from_str(&s).map_err(serde::de::Error::custom)
+    }
+}
+
+impl BorshSerialize for Id {
+    fn serialize<W: std::io::Write>(&self, writer: &mut W) -> std::io::Result<()> {
+        let id_str = self.to_string();
+        BorshSerialize::serialize(&id_str, writer)
+    }
+}
+
+impl BorshDeserialize for Id {
+    fn deserialize_reader<R: std::io::Read>(reader: &mut R) -> std::io::Result<Self> {
+        let id_str: String = BorshDeserialize::deserialize_reader(reader)?;
+        Id::from_str(&id_str).map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))
+    }
+}
+
+impl From<cashu::Id> for Id {
+    fn from(id: cashu::Id) -> Self {
+        Id::from_str(&id.to_string()).expect("cashu::Id renders a valid keyset id")
+    }
+}
+
+impl From<Id> for cashu::Id {
+    fn from(id: Id) -> Self {
+        cashu::Id::from_str(&id.to_string()).expect("ecash::Id renders a valid keyset id")
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, BorshSerialize, BorshDeserialize)]
 pub struct BlindedMessage {
     #[borsh(
         serialize_with = "wire::borsh::serialize_as_u64",
@@ -351,24 +432,126 @@ impl From<MintKeySetInfo> for cashu::KeySetInfo {
     }
 }
 
+// TODO: use from_byte_array_compressed when on secp256k1@0.30
+pub(crate) fn public_key_secp2cashu(pk: &secp::PublicKey) -> cashu::PublicKey {
+    cashu::PublicKey::from_slice(&pk.serialize())
+        .expect("secp256k1::PublicKey <-> cashu::PublicKey")
+}
+
+pub(crate) fn public_key_cashu2secp(pk: &cashu::PublicKey) -> secp::PublicKey {
+    secp::PublicKey::from_slice(&pk.to_bytes()).expect("cashu::PublicKey <-> secp256k1::PublicKey")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::{core, core_tests};
 
-    fn random_keyset_id() -> cashu::Id {
+    fn id_fixtures() -> Vec<String> {
+        vec![
+            String::from("00ad268c4d1f5826"),
+            String::from("000f01df73ea149a"),
+            String::from("00ffffffffffffff"),
+            format!("01{}", "00".repeat(32)),
+            format!("01aabbccddeeff00{}", "11".repeat(25)),
+            format!("01aabbccddeeff00{}", "22".repeat(25)),
+        ]
+    }
+
+    #[test]
+    fn id_json_wire_compat() {
+        for s in id_fixtures() {
+            let id = Id::from_str(&s).expect("parse");
+            assert_eq!(id.to_string(), s);
+
+            let bytes = serde_json::to_vec(&id).expect("serialize");
+            let deserialized: cashu::Id = serde_json::from_slice(&bytes).expect("deserialize");
+            assert_eq!(deserialized, cashu::Id::from(id));
+            assert_eq!(bytes, serde_json::to_vec(&deserialized).expect("serialize"));
+            assert_eq!(bytes, format!("\"{s}\"").into_bytes());
+
+            let back: Id = serde_json::from_slice(&bytes).expect("deserialize");
+            assert_eq!(back, id);
+            assert_eq!(Id::from(cashu::Id::from(id)), id);
+        }
+    }
+
+    #[test]
+    fn id_borsh_wire_compat() {
+        for s in id_fixtures() {
+            let id = Id::from_str(&s).expect("parse");
+            let kid = cashu::Id::from(id);
+
+            let mut buf = Vec::new();
+            BorshSerialize::serialize(&id, &mut buf).expect("serialize");
+            let mut upstream = Vec::new();
+            wire::borsh::serialize_as_str(&kid, &mut upstream).expect("serialize");
+            assert_eq!(buf, upstream);
+            assert_eq!(buf[..4], (s.len() as u32).to_le_bytes());
+
+            let deserialized: Id = BorshDeserialize::deserialize_reader(&mut upstream.as_slice())
+                .expect("deserialize");
+            assert_eq!(deserialized, id);
+            let upstream_back: cashu::Id =
+                wire::borsh::deserialize_from_str(&mut buf.as_slice()).expect("deserialize");
+            assert_eq!(upstream_back, kid);
+        }
+    }
+
+    #[test]
+    fn id_ord_matches_cashu() {
+        let strs = id_fixtures();
+        let mut ids: Vec<Id> = strs
+            .iter()
+            .map(|s| Id::from_str(s).expect("parse"))
+            .collect();
+        let mut kids: Vec<cashu::Id> = strs
+            .iter()
+            .map(|s| cashu::Id::from_str(s).expect("parse"))
+            .collect();
+        ids.sort();
+        kids.sort();
+        assert_eq!(
+            ids.into_iter().map(cashu::Id::from).collect::<Vec<_>>(),
+            kids
+        );
+    }
+
+    #[test]
+    fn id_accept_reject_matches_cashu() {
+        let refused = vec![
+            String::new(),
+            String::from("0"),
+            String::from("00"),
+            String::from("00ad268c4d1f582"),
+            String::from("00ad268c4d1f58267"),
+            format!("01{}", "0".repeat(63)),
+            format!("01{}", "0".repeat(65)),
+            String::from("02ad268c4d1f5826"),
+            String::from("ffad268c4d1f5826"),
+            format!("00{}", "11".repeat(32)),
+            String::from("01ad268c4d1f5826"),
+            String::from("0gad268c4d1f5826"),
+            String::from("00ad268c4d1f582g"),
+            format!("0é{}", "0".repeat(13)),
+        ];
+        for s in refused {
+            assert!(Id::from_str(&s).is_err(), "should refuse {s:?}");
+            assert!(cashu::Id::from_str(&s).is_err(), "cashu refuses {s:?} too");
+        }
+
+        let id = Id::from_str("00AD268C4D1F5826").expect("parse");
+        assert_eq!(id.to_string(), "00ad268c4d1f5826");
+        assert_eq!(
+            id,
+            Id::from(cashu::Id::from_str("00AD268C4D1F5826").expect("parse"))
+        );
+    }
+
+    fn random_keyset_id() -> Id {
         let mut bytes = [0; 8];
         bytes[1..].copy_from_slice(&rand::random::<[u8; 7]>());
-        cashu::Id::from_bytes(&bytes).expect("keyset id")
-    }
-
-    fn random_public_key() -> cashu::PublicKey {
-        cashu::PublicKey::from(core::generate_random_keypair().public_key())
-    }
-
-    fn random_mint_keyset() -> MintKeySet {
-        let (_, keyset) = core_tests::generate_random_ecash_keyset();
-        keyset
+        cashu::Id::from_bytes(&bytes).expect("keyset id").into()
     }
 
     #[test]
